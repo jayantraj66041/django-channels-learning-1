70b53b286a8a9314cd289d8808dc9ed72c7b5b46
Jayant Raj <jayantraj66041@gmail.com>
2022-02-26 14:27:03 +0530

    sync & async connection stablished, sent to client and sisconnect

--- datawork/consumers.py
+++ datawork/consumers.py
@@ -11,6 +11,7 @@
 #             sleep(1)
 
 from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
 
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
@@ -22,9 +23,14 @@ class MySyncConsumer(SyncConsumer):
     def websocket_receive(self, event):
         print("Websocket Received...", event)
         print("Websocket Received  Message -", event['text'])
+        self.send({
+            'type': 'websocket.send',
+            'text': "Message sent to client"
+        })
 
     def websocket_disconnect(self, event):
         print("Websocket Disconnected...", event)
+        raise StopConsumer()
 
 
 class MyAsyncConsumer(AsyncConsumer):
@@ -37,7 +43,12 @@ class MyAsyncConsumer(AsyncConsumer):
     async def websocket_receive(self, event):
         print("Websocket Received...", event)
         print("Websocket Received  Message -", event['text'])
+        await self.send({
+            'type': 'websocket.send',
+            'text': "Message sent to client"
+        })
 
     async def websocket_disconnect(self, event):
         print("Websocket Disconnected...", event)
+        raise StopConsumer()
 
