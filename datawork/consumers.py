f91e2779470d8a66eff86c394f25629ea446a02a
Jayant Raj <jayantraj66041@gmail.com>
2022-02-27 09:52:47 +0530

    understand synchronus and async function difference

--- datawork/consumers.py
+++ datawork/consumers.py
@@ -10,10 +10,14 @@
 #             }))
 #             sleep(1)
 
+
+# Websocket data communication channel stablished
+from time import sleep
+import asyncio
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
 
-class MySyncConsumer(SyncConsumer):
+class MySyncConsumer(SyncConsumer):        # it accessed by one user only at a time
     def websocket_connect(self, event):
         print("Websocket Connected...", event)
         self.send({
@@ -23,17 +27,19 @@ class MySyncConsumer(SyncConsumer):
     def websocket_receive(self, event):
         print("Websocket Received...", event)
         print("Websocket Received  Message -", event['text'])
-        self.send({
-            'type': 'websocket.send',
-            'text': "Message sent to client"
-        })
+        for i in range(1,101):
+            self.send({
+                'type': 'websocket.send',
+                'text': str(i)
+            })
+            sleep(1)
 
     def websocket_disconnect(self, event):
         print("Websocket Disconnected...", event)
         raise StopConsumer()
 
 
-class MyAsyncConsumer(AsyncConsumer):
+class MyAsyncConsumer(AsyncConsumer):   # It accessed by multiple users at a time
     async def websocket_connect(self, event):
         print("Websocket Connected...", event)
         await self.send({
@@ -43,10 +49,12 @@ class MyAsyncConsumer(AsyncConsumer):
     async def websocket_receive(self, event):
         print("Websocket Received...", event)
         print("Websocket Received  Message -", event['text'])
-        await self.send({
-            'type': 'websocket.send',
-            'text': "Message sent to client"
-        })
+        for i in range(1,101):
+            await self.send({
+                'type': 'websocket.send',
+                'text': str(i)
+            })
+            await asyncio.sleep(1)
 
     async def websocket_disconnect(self, event):
         print("Websocket Disconnected...", event)
